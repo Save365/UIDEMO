5178fa2fd84ff35919bca84f2d55e75c526e9d5c
Rachan leevichit <save.save365@icloud.com>
2018-05-22 16:02:09 +0700

    connection

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,49 @@
 package sample;
 
-public class Controller {
-}
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import javax.swing.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    private final String id = "admin";
+    private final String pass = "1234";
+
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button btnLogin;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void Login(ActionEvent event) {
+        try {
+            if (id.equals(username.getText())  && pass.equals(password.getText())){
+                JOptionPane.showMessageDialog(null, "welcome to MIT SaiYai App");
+        }else{
+                JOptionPane.showMessageDialog(null,
+                        "your username or password is invalid",
+                        "warning Massage",
+                        JOptionPane.WARNING_MESSAGE);
+        }
+
+    }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }//login
+}//class
+
